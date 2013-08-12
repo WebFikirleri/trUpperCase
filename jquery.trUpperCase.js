35c9ae4b26d22f6aeaf0381dfca5cd519c4bc83b
WebFikirleri <info@webfikirleri.com>
2013-08-12 16:40:18 +0300

    Update jquery.trUpperCase.js

--- jquery.trUpperCase.js
+++ jquery.trUpperCase.js
@@ -1,14 +1,18 @@
 (function($) {
+  
   $.fn.trUpperCase = function() {
-    var inlineText = $(this).text();
-    inlineText = inlineText.replace(new RegExp('ğ', 'g'),'Ğ');
-    inlineText = inlineText.replace(new RegExp('ü', 'g'),'Ü');
-    inlineText = inlineText.replace(new RegExp('ş', 'g'),'Ş');
-    inlineText = inlineText.replace(new RegExp('i', 'g'),'İ');
-    inlineText = inlineText.replace(new RegExp('ö', 'g'),'Ö');
-    inlineText = inlineText.replace(new RegExp('ç', 'g'),'Ç');
-    inlineText = inlineText.replace(new RegExp('ı', 'g'),'I');
-    inlineText = inlineText.toUpperCase();
-    $(this).text(inlineText);
+    this.each(function() {
+      var inlineText = $(this).text();
+      inlineText = inlineText.replace(new RegExp('ğ', 'g'),'Ğ');
+      inlineText = inlineText.replace(new RegExp('ü', 'g'),'Ü');
+      inlineText = inlineText.replace(new RegExp('ş', 'g'),'Ş');
+      inlineText = inlineText.replace(new RegExp('i', 'g'),'İ');
+      inlineText = inlineText.replace(new RegExp('ö', 'g'),'Ö');
+      inlineText = inlineText.replace(new RegExp('ç', 'g'),'Ç');
+      inlineText = inlineText.replace(new RegExp('ı', 'g'),'I');
+      inlineText = inlineText.toUpperCase();
+      $(this).text(inlineText);
+    });
   };
+
 })(jQuery);
